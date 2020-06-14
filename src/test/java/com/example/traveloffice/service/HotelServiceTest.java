0b5c61c3169918d522bf413b617a0e86bc5230f9
mturczyn15 <mturczyn15@gmail.com>
2020-06-14 13:53:33 +0200

    h2Database

--- src/test/java/com/example/traveloffice/service/HotelServiceTest.java
+++ src/test/java/com/example/traveloffice/service/HotelServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.traveloffice.service;
 
-import com.example.traveloffice.domain.*;
+import com.example.traveloffice.domain.EntityNotFoundException;
+import com.example.traveloffice.domain.Hotel;
+import com.example.traveloffice.domain.HotelDto;
+import com.example.traveloffice.domain.Stars;
 import com.example.traveloffice.mapper.HotelMapper;
 import com.example.traveloffice.repository.HotelOperationRepository;
 import com.example.traveloffice.repository.HotelRepository;
@@ -10,13 +13,13 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.mail.SimpleMailMessage;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -65,15 +68,6 @@ public class HotelServiceTest {
     @Test
     public void testCreate() {
         //Given
-        Mail mail = Mail.builder()
-                .mailTo("test@test.com")
-                .Subject("Test")
-                .message("Test message")
-                .build();
-        SimpleMailMessage simpleMessage = new SimpleMailMessage();
-        simpleMessage.setTo(mail.getMailTo());
-        simpleMessage.setSubject(mail.getSubject());
-        simpleMessage.setText(mail.getMessage());
         Hotel hotel = new Hotel(1L, "task", "city", Stars.FIVE, "3456");
         HotelDto hotelDto = new HotelDto(1L, "task", "city", Stars.FIVE, "3456");
         when(hotelMapper.map(hotelDto)).thenReturn(hotel);
@@ -82,7 +76,7 @@ public class HotelServiceTest {
         //When
         HotelDto createdHotel = hotelService.create(hotelDto);
         //Then
-        assertEquals(1, createdHotel.getId());
+        assertNull(createdHotel.getId());
     }
 
     @Test
