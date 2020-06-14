56f053bb8b6e3a56fab91c8b2e0a2422657a9268
mturczyn15 <mturczyn15@gmail.com>
2020-06-14 13:46:30 +0200

    h2Database

--- src/main/java/com/example/traveloffice/domain/PaymentType.java
+++ src/main/java/com/example/traveloffice/domain/PaymentType.java
@@ -4,6 +4,5 @@ public enum PaymentType {
     BLIK,
     CREDIT_CARD,
     MONEY_TRANSFER,
-    UPON_DELIVERY,
     PAYU
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,3 +14,17 @@ INSERT INTO HOTELS (HOTEL_NAME, CITY, STARS, PHONE_NUMBER)  VALUES
   ('ODYSEJA', 'Kraków', 'FIVE', '123-654-456'),
   ('ZEUS', 'Wrocław', 'TWO', '123-234-456'),
   ('ATENA', 'Lublin', 'FOUR', '123-852-456');
+
+INSERT INTO ADDRESSES (CUSTOMER_ID,
+  STREET, HOUSE_NR, CITY, ZIP_CODE, PHONE_NR)  VALUES
+  ('1', 'Lubelska', '5', 'Warsaw', '22-304', '123-123-456'),
+  ('3', 'Radziszewska', '5c', 'Kraków', '23-123', '566-123-456'),
+  ('4', 'Lwowska', 'Wrocław', '54', '12-345', '345-123-456'),
+  ('2', 'Krótka', 'Lublin', '51', '12-203', '111-123-456');
+
+INSERT INTO BOOKINGS (CUSTOMER_ID, HOTEL_ID, TRAVELAGENCY_ID,
+  PRICE, START_DATE, END_DATE, PAYMENT)  VALUES
+  ('1', '1', '1', '123', '1234-01-01', '1234-01-01', 'BLIK'),
+  ('3', '1', '1', '123', '1234-01-01', '1234-01-01', 'BLIK'),
+  ('4', '1', '1', '123', '1234-01-01', '1234-01-01', 'BLIK'),
+  ('2', '1', '1', '123', '1234-01-01', '1234-01-01', 'BLIK');
